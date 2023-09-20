344e8dc94863f70c52bfebdd10057bcd43103e6b
John <johnromagno@gmail.com>
2023-09-19 21:15:30 -0400

    Good luck trying to fix my shit

---
+++ components/Hi.vue
@@ -0,0 +1,30 @@
+<template>
+<title>
+    <h1>JnR</h1>
+</title>
+
+<header>
+    <h1>John and Ryan</h1>
+    <nav>
+        <ol>
+        <li>click this shit</li>
+        <li>Oi buy shit here</li>
+        <li>Click here to donate to charity (we will pocket this money)</li>
+        </ol>
+    </nav>
+</header>
+</template>
+<script>
+export default {
+  mounted() {
+    this.Hello()
+  },
+  methods: {
+    Hello() {
+        console.log('asjklhdfnksajdnkasdn')
+    }
+  }
+};
+</script>
+
+<style></style>
