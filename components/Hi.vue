8bfc93ef1283974a078cf2126f3a9be5c9268b1e
John <johnromagno@gmail.com>
2023-09-27 17:18:20 -0400

    Added a sorry excuse for nav bar

--- components/Hi.vue
+++ components/Hi.vue
@@ -4,16 +4,22 @@
 </title>
 
 <header>
-    <h1>John and Ryan</h1>
+    <h1>Lil Poggers</h1>
     <nav>
-        <ol>
-        <li>click this shit</li>
-        <li>Oi buy shit here</li>
-        <li>Click here to donate to charity (we will pocket this money)</li>
-        </ol>
+        <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Meet the Team</a></li>
+        <li><a href="#">Contact</a></li>
+        <li class="about" style="float:right"><a class="active" href="#">About</a></li>
+        </ul>
     </nav>
 </header>
 </template>
+
+
+
+
+
 <script>
 export default {
   mounted() {
@@ -21,10 +27,51 @@ export default {
   },
   methods: {
     Hello() {
-        console.log('asjklhdfnksajdnkasdn')
+        console.log('yes')
     }
   }
 };
 </script>
 
-<style></style>
+<style>
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+}
+
+.about {
+  background-color: green;
+}
+
+/* this doesnt work right, changes to grey over hover instead of dark green
+.about:hover {
+  background-color: rgb(0, 37, 0);
+}
+*/
+
+</style>
