486b8ce2d4ccf0ced3de2c063c993a879a4d1515
ryntab <57367794+ryntab@users.noreply.github.com>
2023-09-27 21:44:12 -0400

    refactor:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  pages: true,
   devtools: { enabled: true },
     modules: [
       '@nuxtjs/tailwindcss'
