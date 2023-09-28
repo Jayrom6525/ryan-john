18492666b87e713e292cdc79faa0dc95ef0b4272
ryntab <57367794+ryntab@users.noreply.github.com>
2023-09-27 22:23:16 -0400

    feat:

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-    <Topnav />
-    <slot />
+  <div
+    class="fixed inset-0 flex lg:overflow-hidden antialiased font-sans text-gray-700 dark:text-gray-200 dark:bg-gray-900 select-none"
+  >
+    <div class="flex flex-col flex-1 min-w-0 overflow-hidden">
+      <slot />
+    </div>
   </div>
 </template>
 
 <script>
+export default {};
 </script>
-
-<style>
-body {
-    font-family: Arial, Helvetica, sans-serif;
-  }
-</style>
