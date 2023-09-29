320607c3ebf8d157b49db7cd9a480f170219d8b2
ryntab <57367794+ryntab@users.noreply.github.com>
2023-09-29 18:15:07 -0400

    chore: :rocket: fix deploy

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,5 @@
 <template>
-  <div
-    class="fixed inset-0 flex lg:overflow-hidden antialiased font-sans text-gray-700 dark:text-gray-200 dark:bg-gray-900 select-none"
-  >
-    <div class="flex flex-col flex-1 min-w-0 overflow-hidden">
-      <slot />
-    </div>
-  </div>
+  <slot />
 </template>
 
 <script>
